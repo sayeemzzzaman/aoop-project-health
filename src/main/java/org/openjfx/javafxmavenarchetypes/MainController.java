699c1c419e938673cbc3882a698c651e66b3215c
Sayeem Been Zaman <sayeemzzzaman@gmail.com>
2022-04-12 22:11:15 +0600

    initial commit

---
+++ src/main/java/org/openjfx/javafxmavenarchetypes/MainController.java
@@ -0,0 +1,15 @@
+package org.openjfx.javafxmavenarchetypes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MainController {
+
+
+}
